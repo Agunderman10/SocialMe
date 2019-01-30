977f412fbef215cb602b76658ded79b664cfb966
Andrew Gunderman <agunderman10@gmail.com>
2019-01-30 14:02:29 -0500

    Added public properties

--- SocialMe/ViewModels/MainWindowViewModel.cs
+++ SocialMe/ViewModels/MainWindowViewModel.cs
@@ -15,5 +15,47 @@ namespace SocialMe
         private string _clientMessage = "Type Something...";
         private readonly ObservableCollection<string> _messageHistory = new ObservableCollection<string>();
         #endregion
+        #region Public Properties
+        public string ClientIP
+        {
+            get { return this._clientIP; }
+            set
+            {
+                if (this._clientIP != value)
+                {
+                    this._clientIP = value;
+                }
+            }
+        }
+
+        public string ServerIP
+        {
+            get { return this._serverIP; }
+            set
+            {
+                if (this._serverIP != value)
+                {
+                    this._serverIP = value;
+                }
+            }
+        }
+
+        public string ClientMessage
+        {
+            get { return this._clientMessage; }
+            set
+            {
+                if (this._clientMessage != value)
+                {
+                    this._clientMessage = value;
+                }
+            }
+        }
+
+        public IEnumerable<string> MessageHistory
+        {
+            get { return _messageHistory; }
+        }
+        #endregion
     }
 }
