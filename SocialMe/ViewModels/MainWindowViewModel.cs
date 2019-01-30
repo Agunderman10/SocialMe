b4e640a7c1db1968e5453d15bfb56cf98ab3de10
Andrew Gunderman <agunderman10@gmail.com>
2019-01-30 14:01:32 -0500

    Added private members

--- SocialMe/ViewModels/MainWindowViewModel.cs
+++ SocialMe/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,11 @@ namespace SocialMe
 {
     class MainWindowViewModel
     {
-
+        #region Private Members
+        private string _clientIP;
+        private string _serverIP;
+        private string _clientMessage = "Type Something...";
+        private readonly ObservableCollection<string> _messageHistory = new ObservableCollection<string>();
+        #endregion
     }
 }
