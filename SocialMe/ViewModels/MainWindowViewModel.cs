a3294e7446314bed0abaaea21ad6d87ba2aebeb1
Andrew Gunderman <agunderman10@gmail.com>
2019-01-31 04:39:23 -0500

    Removed unecessary usings

--- SocialMe/ViewModels/MainWindowViewModel.cs
+++ SocialMe/ViewModels/MainWindowViewModel.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace SocialMe
