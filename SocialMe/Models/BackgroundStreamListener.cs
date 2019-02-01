039bf3205dba24260815aec13a8e90968b02dc86
Andrew Gunderman <agunderman10@gmail.com>
2019-02-01 11:14:47 -0500

    Added listener

--- SocialMe/Models/BackgroundStreamListener.cs
+++ SocialMe/Models/BackgroundStreamListener.cs
@@ -10,8 +10,8 @@
         {
             NetworkStream netStream = _client.GetStream();
 
-            //while (_client.Connected)
-            //{
+            while (_client.Connected)
+            {
                 if (netStream.CanRead)
                 {
                     byte[] bytes = new byte[_client.ReceiveBufferSize];
@@ -27,7 +27,7 @@
                     netStream.Close();
                     return;
                 }
-            //}
+            }
         }
     }
 }
