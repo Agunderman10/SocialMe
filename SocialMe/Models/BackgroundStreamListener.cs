d7f850500d3a9b5caa8f088bf3d63789e209c7cd
Andrew Gunderman <agunderman10@gmail.com>
2019-02-01 11:46:00 -0500

    Reset head

--- SocialMe/Models/BackgroundStreamListener.cs
+++ SocialMe/Models/BackgroundStreamListener.cs
@@ -5,26 +5,27 @@
 
     class BackgroundStreamListener
     {
-        //we run this listener on a background thread, listerning for messages
+        //we run this listener on a background thread, listening for messages
         public void RunMessageListener(TcpClient _client, MainWindowViewModel _mainWindowViewModel)
         {
             NetworkStream netStream = _client.GetStream();
 
-            if (netStream.CanRead)
-            {
-                byte[] bytes = new byte[_client.ReceiveBufferSize];
-                netStream.Read(bytes, 0, (int)_client.ReceiveBufferSize);
+                if (netStream.CanRead)
+                {
+                    byte[] bytes = new byte[_client.ReceiveBufferSize];
+                    netStream.Read(bytes, 0, (int)_client.ReceiveBufferSize);
 
-                string receivedMessage = Encoding.UTF8.GetString(bytes);
-                _mainWindowViewModel.DisplayMessage(receivedMessage);
-            }
-            else
-            {
-                _mainWindowViewModel.DisplayErrorMessage();
-                _client.Close();
-                netStream.Close();
-                return;
-            }
+                    string receivedMessage = Encoding.UTF8.GetString(bytes);
+                    _mainWindowViewModel.DisplayMessage(receivedMessage);
+                }
+                else
+                {
+                    _mainWindowViewModel.DisplayErrorMessage();
+                    _client.Close();
+                    netStream.Close();
+                    return;
+                }
+            
         }
     }
 }
