b915c6782564c212fc93bccb2124b3dca416416a
Andrew Gunderman <agunderman10@gmail.com>
2019-02-08 19:29:28 -0500

    Removed misleading member

--- SocialMe/Models/BackgroundStreamListener.cs
+++ SocialMe/Models/BackgroundStreamListener.cs
@@ -6,21 +6,21 @@
     class BackgroundStreamListener
     {
         //we run this listener on a background thread, listening for messages
-        public void RunMessageListener(TcpClient _client, MainWindowViewModel _mainWindowViewModel)
+        public void RunMessageListener(TcpClient client, MainWindowViewModel mainWindowViewModel)
         {
-            NetworkStream netStream = _client.GetStream();
+            NetworkStream netStream = client.GetStream();
 
                 while(netStream.CanRead)
                 {
-                    byte[] bytes = new byte[_client.ReceiveBufferSize];
-                    netStream.Read(bytes, 0, (int)_client.ReceiveBufferSize);
+                    byte[] bytes = new byte[client.ReceiveBufferSize];
+                    netStream.Read(bytes, 0, (int)client.ReceiveBufferSize);
 
                     string receivedMessage = Encoding.UTF8.GetString(bytes);
-                    _mainWindowViewModel.DisplayMessage(receivedMessage);
+                    mainWindowViewModel.DisplayMessage(receivedMessage);
                 }
                
-                _mainWindowViewModel.DisplayErrorMessage();
-                _client.Close();
+                mainWindowViewModel.DisplayErrorMessage();
+                client.Close();
                 netStream.Close();
                 return;            
         }
